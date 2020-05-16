7a2b0002fc7d9739b19ccc66198bcf1876ac716e
ad-sho-loko <ad.sho.loko@gmail.com>
2020-05-17 03:18:28 +0900

    able to read utf8

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
 	"syscall"
+	"unicode/utf8"
 )
 
+// Key Definitions
 const (
 	ControlC = 3
 )
@@ -14,13 +16,14 @@ const (
 type Editor struct {
 	fileName string
 	rawState *terminal.State
+	keyChan chan rune
+	row int
+	col int
 	width int
 	height int
 }
 
-var e = &Editor{}
-
-func initTerminal() {
+func (e *Editor) initTerminal() {
 	state, err := terminal.MakeRaw(0)
 	if err != nil {
 		panic(err)
@@ -36,41 +39,77 @@ func initTerminal() {
 	e.rawState = state
 }
 
-func readKey(keyCh chan []byte) {
-	buf := make([]byte, 256)
+func (e *Editor) flush() {
+	e.write([]byte("\033[2J"))
+}
+
+func (e *Editor) moveCursor(row, col int) {
+	s := fmt.Sprintf("\033[%d;%dH",row, col)
+	e.write([]byte(s))
+}
+
+func (e *Editor) write(b []byte) {
+	syscall.Write(0, b)
+}
+
+func (e *Editor) writeUtf8() {
+}
+
+func (e *Editor) readKeys() {
+	buf := make([]byte, 64)
 
 	for {
 		if n, err := syscall.Read(0, buf); err == nil {
-			keyCh <- buf[:n]
+			b := buf[:n]
+			for {
+				r, n := utf8.DecodeRune(b)
+				if n == 0 {
+					break
+				}
+				e.keyChan <- r
+				b = b[n:]
+			}
 		}
 	}
 }
 
-func handleEvents() {
+func (e *Editor) handleEvents() {
 }
 
-func restoreTerminal() {
+func (e *Editor) restoreTerminal() {
 	if err := terminal.Restore(0, e.rawState); err != nil {
 		panic("Cannot restore from raw mode.")
 	}
 }
 
-func run(fileName string) {
-	e.fileName = fileName
-	initTerminal()
-
-	keyCh := make(chan []byte)
-	go readKey(keyCh)
-
+func (e *Editor) interpret() {
 	for {
-		ch := <- keyCh
-		fmt.Println(ch)
-
-		if ch[0] == ControlC {
-			restoreTerminal()
+		char := <- e.keyChan
+		switch char {
+		case ControlC:
+			e.restoreTerminal()
 			return
+
+		default:
+			fmt.Println(string(char))
 		}
 	}
+}
+
+func run(fileName string) {
+	e := &Editor{
+		row: 0,
+		col: 0,
+		fileName: fileName,
+		keyChan: make(chan rune),
+	}
+
+	e.initTerminal()
+	e.flush()
+	e.moveCursor(0, 0)
+
+	go e.readKeys()
+	e.interpret()
 }
 
 func main() {
