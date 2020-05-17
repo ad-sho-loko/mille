217fb1cd9f4e3e939228f4cf44bccf9693562c11
ad-sho-loko <ad.sho.loko@gmail.com>
2020-05-18 00:19:44 +0900

    improve backspace behaivor

--- main.go
+++ main.go
@@ -4,24 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
+	"io/ioutil"
+	"strings"
 	"syscall"
 	"unicode/utf8"
 )
 
 // Key Definitions
 const (
-	ControlB  = 2
-	ControlC  = 3
-	ControlF  = 6
-	ControlH  = 8
-	Enter     = 13
-	ControlN  = 14
-	ControlP  = 16
-	BackSpace = 127
-	ArrowUp = 1000
-	ArrowDown = 1001
+	ControlB   = 2
+	ControlC   = 3
+	ControlF   = 6
+	ControlH   = 8
+	Enter      = 13
+	ControlN   = 14
+	ControlP   = 16
+	ControlS   = 19
+	BackSpace  = 127
+	ArrowUp    = 1000
+	ArrowDown  = 1001
 	ArrowRight = 1002
-	ArrowLeft = 1003
+	ArrowLeft  = 1003
 )
 
 type Editor struct {
@@ -36,7 +39,7 @@ type Editor struct {
 }
 
 type Row struct {
-	n int
+	numberOfRunes int
 	runes []rune
 }
 
@@ -70,7 +73,7 @@ func (e *Editor) flush() {
 	e.write([]byte("\033[2J"))
 }
 
-func (e *Editor) flushRow(row int){
+func (e *Editor) flushRow() {
 	e.write([]byte("\033[2K"))
 }
 
@@ -90,7 +93,7 @@ func (e *Editor) writeRow(r *Row) {
 		buf = append(buf, []byte(string(s))...)
 	}
 
-	e.flushRow(e.crow)
+	e.flushRow()
 	e.moveCursor(e.crow, 0)
 	e.write(buf)
 }
@@ -109,7 +112,9 @@ func (e *Editor) deleteAt(row *Row, col int) {
 		}
 	}
 
+	row.numberOfRunes -= 1
 	row.runes = newRune
+
 	e.writeRow(row)
 }
 
@@ -145,8 +150,11 @@ func (e *Editor) setRowCol(row int, col int) {
 	e.moveCursor(e.crow, e.ccol)
 }
 
+func (e *Editor) numberOfRunesInRow() int { return e.rows[e.crow].numberOfRunes }
+
 func (e *Editor) appendChar(row int, r rune) {
-	e.rows[row].runes = append(e.rows[row].runes, r)
+	e.rows[row].numberOfRunes += 1
+	e.rows[row].runes[e.ccol] = r
 	e.write([]byte(string(r)))
 }
 
@@ -156,15 +164,41 @@ func (e *Editor) backspace() {
 		e.deleteAt(row, e.ccol-1)
 		e.setRowCol(e.crow, e.ccol-1)
 	} else {
-		e.setRowCol(e.crow-1, e.ccol)
+		e.setRow(e.crow - 1)
+		e.setCol(e.numberOfRunesInRow() - 1)
+	}
+}
+
+func (e *Editor) next() {
+	if e.ccol >= e.rows[e.crow].numberOfRunes {
+		e.setRowCol(e.crow+1, 0)
+	} else {
+		e.setRowCol(e.crow, e.ccol+1)
 	}
 }
 
 func (e *Editor) enter() {
+	e.appendChar(e.crow, '\n')
 	e.setRowCol(e.crow+1, 0)
 }
 
-func (e *Editor) parseKey(b []byte) (rune, int){
+func (e *Editor) saveFile() {
+	sb := strings.Builder{}
+
+	for _, r := range e.rows {
+		if r.numberOfRunes >= 1 {
+			for _, ch := range r.runes {
+				if ch == rune(byte(0)) { continue }
+				sb.WriteRune(ch)
+			}
+			sb.WriteByte('\n')
+		}
+	}
+
+	ioutil.WriteFile("tmp", []byte(sb.String()), 0644)
+}
+
+func (e *Editor) parseKey(b []byte) (rune, int) {
 	// try parsing escape sequence
 	if len(b) == 3 {
 		if b[0] == byte(27) && b[1] == '[' {
@@ -218,7 +252,7 @@ func (e *Editor) interpretKey() {
 			return
 
 		case ControlF, ArrowRight:
-			e.setRowCol(e.crow, e.ccol+1)
+			e.next()
 
 		case ControlH, BackSpace:
 			e.backspace()
@@ -228,6 +262,9 @@ func (e *Editor) interpretKey() {
 
 		case Enter:
 			e.enter()
+
+		case ControlS:
+			e.saveFile()
 
 		case ControlP, ArrowUp:
 			e.setRowCol(e.crow-1, e.ccol)
@@ -239,16 +276,19 @@ func (e *Editor) interpretKey() {
 	}
 }
 
-func run(fileName string) {
-	// TODO: バッファの初期化
+func makeRows() []*Row {
 	rows := make([]*Row, 16)
 	for i := range rows {
 		rows[i] = &Row{
-			n: i + 1,
-			runes: []rune{},
+			numberOfRunes:0,
+			runes: make([]rune, 128),
 		}
 	}
+	return rows
+}
 
+func run(fileName string) {
+	rows := makeRows()
 	e := &Editor{
 		crow:     0,
 		ccol:     0,
---
+++ main_test.go
@@ -0,0 +1,6 @@
+package main
+
+import "testing"
+
+func testDeleteAt(t *testing.T) {
+}
