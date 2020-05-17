9030b963ef116dafea8ffc26a09f8dd4dc4f5d7c
ad-sho-loko <ad.sho.loko@gmail.com>
2020-05-17 14:05:20 +0900

    implement some keys.

--- main.go
+++ main.go
@@ -10,19 +10,32 @@ import (
 
 // Key Definitions
 const (
-	ControlC = 3
+	ControlB  = 2
+	ControlC  = 3
+	ControlF  = 6
+	ControlH  = 8
+	Enter     = 13
+	ControlN  = 14
+	ControlP  = 16
+	BackSpace = 127
 )
 
 type Editor struct {
 	fileName string
 	rawState *terminal.State
-	keyChan chan rune
-	row int
-	col int
-	width int
-	height int
+	keyChan  chan rune
+	crow     int
+	ccol     int
+	rows     []*Row
+	width    int
+	height   int
 }
 
+type Row struct {
+	chars []string
+}
+
+// Terminal
 func (e *Editor) initTerminal() {
 	state, err := terminal.MakeRaw(0)
 	if err != nil {
@@ -37,14 +50,23 @@ func (e *Editor) initTerminal() {
 	e.width = width
 	e.height = height
 	e.rawState = state
+	e.flush()
+	e.moveCursor(e.crow, e.ccol)
 }
 
+func (e *Editor) restoreTerminal() {
+	if err := terminal.Restore(0, e.rawState); err != nil {
+		panic("Cannot restore from raw mode.")
+	}
+}
+
+// Views
 func (e *Editor) flush() {
 	e.write([]byte("\033[2J"))
 }
 
 func (e *Editor) moveCursor(row, col int) {
-	s := fmt.Sprintf("\033[%d;%dH",row, col)
+	s := fmt.Sprintf("\033[%d;%dH", row, col)
 	e.write([]byte(s))
 }
 
@@ -52,7 +74,49 @@ func (e *Editor) write(b []byte) {
 	syscall.Write(0, b)
 }
 
-func (e *Editor) writeUtf8() {
+// Models
+func (e *Editor) setRow(row int) {
+	if row < 1 {
+		row = 1
+	}
+
+	if row >= e.height {
+		row = e.height
+	}
+
+	e.crow = row
+	e.moveCursor(e.crow, e.ccol)
+}
+
+func (e *Editor) setCol(col int) {
+	if col < 1 {
+		col = 1
+	}
+
+	if col >= e.width {
+		col = e.width
+	}
+
+	e.ccol = col
+	e.moveCursor(e.crow, e.ccol)
+}
+
+func (e *Editor) setRowCol(row int, col int) {
+	e.setRow(row)
+	e.setCol(col)
+	e.moveCursor(e.crow, e.ccol)
+}
+
+func (e *Editor) backspace() {
+	if e.ccol > 1 {
+		e.setRowCol(e.crow, e.ccol-1)
+	} else {
+		e.setRowCol(e.crow-1, e.ccol)
+	}
+}
+
+func (e *Editor) enter() {
+	e.setRowCol(e.crow+1, 0)
 }
 
 func (e *Editor) readKeys() {
@@ -73,43 +137,52 @@ func (e *Editor) readKeys() {
 	}
 }
 
-func (e *Editor) handleEvents() {
-}
-
-func (e *Editor) restoreTerminal() {
-	if err := terminal.Restore(0, e.rawState); err != nil {
-		panic("Cannot restore from raw mode.")
-	}
-}
-
-func (e *Editor) interpret() {
+func (e *Editor) interpretKey() {
 	for {
-		char := <- e.keyChan
-		switch char {
+		r := <-e.keyChan
+
+		switch r {
+		case ControlB:
+			e.setRowCol(e.crow, e.ccol-1)
+
 		case ControlC:
 			e.restoreTerminal()
 			return
 
+		case ControlF:
+			e.setRowCol(e.crow, e.ccol+1)
+
+		case ControlH, BackSpace:
+			e.backspace()
+
+		case ControlN:
+			e.setRowCol(e.crow+1, e.ccol)
+
+		case Enter:
+			e.enter()
+
+		case ControlP:
+			e.setRowCol(e.crow-1, e.ccol)
+
 		default:
-			fmt.Println(string(char))
+			fmt.Print(string(r))
+			e.setCol(e.ccol + 1)
 		}
 	}
 }
 
 func run(fileName string) {
 	e := &Editor{
-		row: 0,
-		col: 0,
+		crow:     1,
+		ccol:     1,
+		rows:     []*Row{},
 		fileName: fileName,
-		keyChan: make(chan rune),
+		keyChan:  make(chan rune),
 	}
 
 	e.initTerminal()
-	e.flush()
-	e.moveCursor(0, 0)
-
 	go e.readKeys()
-	e.interpret()
+	e.interpretKey()
 }
 
 func main() {
